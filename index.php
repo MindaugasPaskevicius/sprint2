ab5be5742988d95687cc0620eeb6a9356c0a61bd
MindaugasPaskevicius <106962850+MindaugasPaskevicius@users.noreply.github.com>
2022-09-06 12:40:29 +0300

    create back button

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 <html>
 
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css">
     <title>File System Browser</title>
 </head>
 
-<body>
+<body class="container">
     <?php
     $path = isset($_GET["path"]) ? './' . $_GET["path"] : './';
     $files_and_dirs = scandir($path);
 
-    print('<h2>Directory contents: ' . str_replace('?path=', '', $_SERVER['REQUEST_URI']) . '</h2>');
+    print('<h2>Folder name: ' . str_replace('?path=', '', $_SERVER['REQUEST_URI']) . '</h2>');
 
     // List all files and directories
     print('<table><th>Type</th><th>Name</th><th>Actions</th>');
@@ -30,6 +31,7 @@
         }
     }
     print("</table>");
+    print ("<button>BACK</button>");
     ?>
 </body>
 
